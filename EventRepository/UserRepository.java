ad015e48b8bae10076acad8555df592366869c8f
pranitas14 <pranitayshinde@gmail.com>
2024-08-13 18:11:05 +0530

    Delete EventRepository directory

--- EventRepository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Event.Management.EventRepository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.example.Event.Management.Entity.User;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
